30aee855f26ddaf924e64688c8ea8dc9e22f45d6
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-14 19:14:31 +0500

    yang

--- apps/urls.py
+++ apps/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, product_detail
 
 urlpatterns = [
-    path('/', index, name='menu'),
+    path('', index, name='menu'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
 
