b89e56d615cea7b2df161c4ee6f18bc176edb3f8
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-14 17:31:29 +0500

    yang

--- root/settings.py
+++ root/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-hr3ye%(avz5)7r_1@6qv+3#gz8da*a^nvao!d_bl5cvox8c97a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
