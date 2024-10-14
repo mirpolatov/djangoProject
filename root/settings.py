50582863b24b042339cd83aa3deb4fc7c0b89798
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-14 17:20:05 +0500

    yang

--- root/settings.py
+++ root/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_BASE_DIR = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
