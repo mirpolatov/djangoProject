1c7a232be3048cbf233d567005d0b34573eb3cb4
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-14 17:38:08 +0500

    yang

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, product_detail
 
 urlpatterns = [
-    path('', index, name='index'),  # This maps the root URL to the index view
+    path('', index, name='index'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
+
--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from apps.models import Product
 
@@ -8,3 +8,8 @@ from apps.models import Product
 def index(request):
     products = Product.objects.all()  # Fetch all products with images
     return render(request, 'index.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
